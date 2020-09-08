23620ee054139a1f9453080fe6c987d2e678f8e3
EkramyNo3man <Ekramy.no3man@gmail.com>
2020-09-08 12:58:46 +0200

    install mongoos

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) =>
 
 app.post('/video-request', async (req, res, next) => {
   const response = await VideoRequestData.createRequest(req.body);
-  res.send(response);
+  res.redirect('http://127.0.0.1:5500/');
   next();
 });
 
